307c88ff322f0c84bbb739984b4c6eef0732b705
douglas.biserra <douglas.beserra@hotmail.com>
2022-06-13 10:43:10 -0300

    teste db!!

--- src/database.js
+++ src/database.js
@@ -1,18 +1,40 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
 
-function connectToDatabase(){
-mongoose.connect(process.env.DATABASE_URL,
-{
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
+// function connectToDatabase(){
+// mongoose.connect(process.env.DATABASE_URL,
+// {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+// });
 
 
-const db = mongoose.connection;
-db.on("error", (error) => console.error(error));
-db.once("open", () => console.log("Connected to the database!"));
+// const db = mongoose.connection;
+// db.on("error", (error) => console.error(error));
+// db.once("open", () => console.log("Connected to the database!"));
+
+// }
+
+// module.exports = connectToDatabase;
+
+
+import mongoose from 'mongoose';
+
+const connectDB =  async ()=>{
+
+    try{
+        const conn = await mongoose.connect(process.env.DATABASE_URL,{
+            //must add in order to not get any error masseges:
+            useUnifiedTopology:true,
+            useNewUrlParser: true,
+            useCreateIndex: true
+        })
+        console.log(`mongo database is connected!!! ${conn.connection.host} `)
+    }catch(error){
+        console.error(`Error: ${error} `)
+        process.exit(1) //passing 1 - will exit the proccess with error
+    }
 
 }
 
-module.exports = connectToDatabase;
+export default connectDB
--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
-const path = require('path');
+// require('dotenv').config({ path: process.env.DATABASE_URL });
 
-
-require('dotenv').config({ path: process.env.DATABASE_URL });
+dotenv.config()
+const port = process.env.PORT || 3333
+const mode = process.env.DATABASE_URL
 
 const express = require("express");
 const mongoose = require("mongoose");
@@ -16,7 +17,7 @@ const connectToDatabase = require("./database")
 connectToDatabase();
 
 const app = express();
-const port = process.env.PORT|| 3333;
+// const port = process.env.PORT|| 3333;
 
 app.use(express.json());
 app.use(routes);
