648f2604c50cff8a65b9a4df4f26cd840ff6efe2
douglas.biserra <douglas.beserra@hotmail.com>
2022-06-15 10:41:18 -0300

    cors teste

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 require('dotenv').config()
+const cors = require('cors');
 const port = process.env.PORT || 3333
 const mode = process.env.DATABASE_URL
 const express = require("express");
@@ -8,7 +9,14 @@ const connectToDatabase = require("./database")
 connectToDatabase();
 
 
-
+app.use((request, response, next) => {
+	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+    response.header("Access-Control-Allow-Origin", "*");
+	//Quais são os métodos que a conexão pode realizar na API
+    response.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    app.use(cors());
+    next();
+});
 
 
 const app = express();
